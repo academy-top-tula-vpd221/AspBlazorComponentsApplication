ebb841f8db9bab48e38b34b934248fc42edca3dd
maxefimov <efimov.top.academy@gmail.com>
2024-04-14 18:02:33 +0300

    Добавьте файлы проекта.

---
+++ AspBlazorComponentsApplication/Program.cs
@@ -0,0 +1,17 @@
+using AspBlazorComponentsApplication.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
